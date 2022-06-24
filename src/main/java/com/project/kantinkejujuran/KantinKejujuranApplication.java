077b828688e53135cad73771c5819addd4e66194
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-06-24 15:59:27 +0700

    Initial commit

---
+++ src/main/java/com/project/kantinkejujuran/KantinKejujuranApplication.java
@@ -0,0 +1,13 @@
+package com.project.kantinkejujuran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KantinKejujuranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KantinKejujuranApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/kantinkejujuran/KantinKejujuranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.kantinkejujuran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class KantinKejujuranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
