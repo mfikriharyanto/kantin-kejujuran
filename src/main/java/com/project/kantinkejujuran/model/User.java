36f44f1f3aed7007aa28928efcb4d75c87ad4c2c
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-06-27 17:23:42 +0700

    Add import javax persistence enumerated and enumtype

--- src/main/java/com/project/kantinkejujuran/model/User.java
+++ src/main/java/com/project/kantinkejujuran/model/User.java
@@ -12,6 +12,8 @@ import java.util.Collection;
 import java.util.Collections;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
