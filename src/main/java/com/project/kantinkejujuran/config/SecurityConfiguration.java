bf8e3a5310d0e4a2656653a1f2877a1180a35420
Muhammad Fikri Haryanto <muhammad.fikri06@ui.ac.id>
2022-06-29 21:38:23 +0700

    Update security configuration

--- src/main/java/com/project/kantinkejujuran/config/SecurityConfiguration.java
+++ src/main/java/com/project/kantinkejujuran/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/store").permitAll()
                 .antMatchers("/registration").anonymous()
                 .anyRequest().authenticated()
                 .and()
